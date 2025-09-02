c64363b1c4bb7d7686eb53bb85b63b9b9de8330e
K1dou <hique1276@gmail.com>
2025-09-02 00:43:33 -0300

    add POST route to create new posts with authentication

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -10,4 +10,22 @@ export async function postsRoutes(app) {
   app.get('/posts', { onRequest: [isAuth] }, (request, reply) => {
     return reply.status(200).send(posts);
   });
+
+  app.post('/posts', { onRequest: [isAuth] }, (request, reply) => {
+    const { username, title, content } = request.body;
+
+    const post = {
+      id: posts.length + 1,
+      owner: username,
+      title,
+      content,
+      date: new Date().toISOString(),
+      comments: [],
+      likes: [],
+    };
+
+    posts.push(post);
+
+    return reply.status(201).send(post);
+  });
 }
