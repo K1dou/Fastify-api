f5d1ed1f802740bc3b4c8cb010bc56be12fc9034
K1dou <hique1276@gmail.com>
2025-09-02 00:56:02 -0300

    add POST route to add comments to existing posts

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -11,6 +11,25 @@ export async function postsRoutes(app) {
     return reply.status(200).send(posts);
   });
 
+  app.post('/posts/:id/comment', { onRequest: [isAuth] }, (request, reply) => {
+    const { id } = request.params;
+    const { username, content } = request.body;
+
+    const postIndex = posts.findIndex((post) => post.id === parseInt(id));
+
+    if (postIndex === -1) {
+      return reply.status(404).send({ message: 'Post not found' });
+    }
+    const comment = {
+      owner: username,
+      content,
+      date: new Date().toISOString(),
+    };
+    posts[postIndex].comments.push(comment);
+
+    return reply.status(201).send(comment);
+  });
+
   app.post('/posts', { onRequest: [isAuth] }, (request, reply) => {
     const { username, title, content } = request.body;
 
